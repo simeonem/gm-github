88c5dd61ebe448aa6bbe46bd4aa5094736e9140d
Carpenter Marino <simeonem@umich.edu>
2016-05-03 15:57:51 -0400

    I win again.

--- read_gm_data.R
+++ read_gm_data.R
@@ -4,10 +4,8 @@ gdp <- rbind(gdp, new)
 
 #gdp$gdp <- gdp$gdpPercap * gdp$pop
 
-<<<<<<< HEAD
 #calculating gdp by qeoakes
 gdp$gdp <- gdp$gdpPercap * gdp$pop
->>>>>>> 0be63a6e58951ba5836e3be710e51d0063cae8fc
 
 keep <- c('country','year','lifeExp','gdp')
 gdp <- gdp[keep]
